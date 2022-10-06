db805cc394768667b006b40cd764e9f88ec4a5a9
HushchinVolodymyr <114146250+HushchinVolodymyr@users.noreply.github.com>
2022-10-06 12:31:01 +0300

    Update my_task_1_1.py

--- my_task_1_1.py
+++ my_task_1_1.py
@@ -1,15 +1,17 @@
-import argparse
+mport argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("number1", type=float)
+parser.add_argument("number1", type=int)
 parser.add_argument("operator", type=str)
-parser.add_argument("number2", type=float)
+parser.add_argument("number2", type=int)
 
 args = parser.parse_args()
 
-if args.operator in ("+", "-", "*", "/"):
+if args.number2 == 0 and args.operator in ("/"):
+    print("Сannot be divided by zero")
+elif args.operator in ("+", "-", "*", "/"):
     print(args)
     print(eval(str(args.number1) + args.operator + str(args.number2)))
 else:
-    print("Not math symbol")
+    print(" or Not math symbol")
