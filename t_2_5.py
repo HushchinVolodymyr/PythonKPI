517fbf730f5d6203c6720133359563a688d28039
HushchinVolodymyr <114146250+HushchinVolodymyr@users.noreply.github.com>
2022-10-13 00:19:52 +0300

    Update t_2_5.py

--- t_2_5.py
+++ t_2_5.py
@@ -47,24 +47,3 @@ class Group:
             top.append(stud)
 
         return top
-
-first_student = Student("name_1", "surname_1", "inp", [2, 4, 6, 3, 6, 4])
-second_student = Student("name_2", "surname_2", "inp", [2, 4, 5, 3, 2, 4]) 
-third_student = Student("name_3", "surname_3", "inp", [4, 4, 6, 7, 2, 4]) 
-fourth_student = Student("name_4", "surname_4", "inp", [2, 4, 6, 3, 2, 4]) 
-fivth_student = Student("name_5", "surname_5", "inp", [2, 4, 6, 3, 9, 2]) 
-sixth_student = Student("name_6", "surname_6", "inp", [2, 4, 5, 3, 2, 4]) 
-seventh_student = Student("name_7", "surname_7", "inp", [2, 9, 4, 3, 2, 4]) 
-eighth_student = Student("name_8", "surname_8", "inp", [3, 4, 6, 3, 2, 1]) 
-ninth_student = Student("name_9", "surname_9", "inp", [2, 4, 6, 9, 2, 4]) 
-tenth_student = Student("name_10", "surname_10", "inp", [1, 4, 6, 3, 2, 5])
-
-all_students = [first_student, second_student, third_student, fourth_student, fivth_student,
-sixth_student, seventh_student, eighth_student, ninth_student, tenth_student]
-
-group = Group(all_students)
-
-top_student = group.top_5_students()
-
-for el in top_student:
-    print(el.name)
